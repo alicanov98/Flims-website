ff816c2db1410cf274a13e9b53a8930c1a22fbb4
alicanov98 <alicanov1998@gmail.com>
2023-11-14 02:26:34 +0400

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+import "@/styles/global.css";
+import "@/styles/reset.css"
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head/>
+      <body >{children}</body>
+    </html>
+  )
+}
